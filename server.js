b2042d70d0b95cfeb69bb2fc947d52102bfb0aeb
edupoly <info.edupoly@gmail.com>
2026-06-09 17:54:32 +0530

    file-upload

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ require("dotenv").config();
 const express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    console.log("inside storage::", file);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "_" + file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
 var cors = require("cors");
 var arthRouter = require("./routes/arth.routes");
 var empRouter = require("./routes/emp.routes");
@@ -13,8 +27,10 @@ const { authenticate } = require("./middlewares/auth.middleware");
 dbConnect();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use(express.static(__dirname + "/public"));
 
 app.use("/auth", authRouter);
@@ -26,6 +42,11 @@ app.use("/employees", empRouter);
 app.use("/fruits", fruitsRouter);
 
 app.use("/todos", authenticate, todoRouter);
+
+app.post("/uploadFile", upload.single("profilepic"), (req, res) => {
+  console.log("req for uploadFile", req.body);
+  res.send("Lets see");
+});
 
 app.listen(process.env.PORT || 3500, () => {
   console.log(`server running on ${process.env.PORT || 3500}`);
