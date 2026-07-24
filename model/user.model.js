da8055a3ada724082fd6dab1e207ff6fdb01d565
edupoly <info.edupoly@gmail.com>
2026-07-24 20:31:36 +0530

    gAuthemo

--- model/user.model.js
+++ model/user.model.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose");
 var userSchema = mongoose.Schema({
   username: String,
   password: String,
+  email: String,
+  picture: String,
 });
 
 var UserModel = mongoose.model("user", userSchema);
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const { OAuth2Client } = require("google-auth-library");
+const jwt = require("jsonwebtoken");
+
 var app = express();
 var bodyParser = require("body-parser");
 var cors = require("cors");
@@ -10,13 +13,62 @@ var authRouter = require("./routes/auth.routes");
 var todoRouter = require("./routes/todo.routes");
 var dbConnect = require("./db");
 const { authenticate } = require("./middlewares/auth.middleware");
+
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 dbConnect();
+
+var UserModel = require("./model/user.model");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
+app.post("/api/auth/google", (req, res) => {
+  const { token } = req.body;
+  // Verify the token with Google and extract user information
+  // You can use a library like google-auth-library for this purpose
+  // After verification, you can create or find the user in your database and return a response
+
+  client
+    .verifyIdToken({
+      idToken: token,
+      audience:
+        "733215085063-2fh9hojuqr37l9hn0fsajjrurlvbovb2.apps.googleusercontent.com",
+    })
+    .then((ticket) => {
+      const payload = ticket.getPayload();
+      const userid = payload["sub"];
+      console.log("Google user ID:", userid);
+      console.log("ticket payload:", payload);
+      var { email, name, picture } = payload;
+
+      //  DB Logic:
+      UserModel.findOne({ email }).then((user) => {
+        if (!user) {
+          UserModel.create({ email, username: name, picture }).then((data) => {
+            console.log("New user created:", data);
+          });
+        }
+      });
+
+      const appToken = jwt.sign(
+        { email, username: name },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "1d",
+        },
+      );
+      res.send({
+        msg: "success",
+        username: name,
+        email,
+        picture,
+        token: appToken,
+      });
+    });
+});
 app.use("/auth", authRouter);
 
 app.use("/arth", arthRouter);
